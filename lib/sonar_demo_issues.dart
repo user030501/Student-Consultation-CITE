06305fcafdf516fbceb9fa840660086f0635be49
charles <charles.ponto@jmc.edu.ph>
2026-04-15 11:17:34 +0800

    fix sonar issues

---
+++ lib/sonar_demo_issues.dart
@@ -0,0 +1,8 @@
+class sonarDemoTracker {
+  final String status = 'Open';
+}
+
+String buildSonarDemoLabel() {
+  final issueMessage = 'Intentional SonarQube demo issue';
+  return 'Demo issues enabled';
+}
